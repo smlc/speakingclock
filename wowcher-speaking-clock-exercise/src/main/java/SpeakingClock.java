1983e81b10e5f26b592a633f9d3178b51603ce95
smlc <siesa.marlon@hotmail.fr>
2020-07-19 20:02:03 +0100

    nominal case hour to text

--- wowcher-speaking-clock-exercise/src/main/java/SpeakingClock.java
+++ wowcher-speaking-clock-exercise/src/main/java/SpeakingClock.java
@@ -1,3 +1,71 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class SpeakingClock {
 
+  private static final String[] num = {
+      "",
+      " one",
+      " two",
+      " three",
+      " four",
+      " five",
+      " six",
+      " seven",
+      " eight",
+      " nine",
+      " ten",
+      " eleven",
+      " twelve",
+      " thirteen",
+      " fourteen",
+      " fifteen",
+      " sixteen",
+      " seventeen",
+      " eighteen",
+      " nineteen"
+  };
+
+  private static final String[] tens = {
+      "",
+      " ten",
+      " twenty",
+      " thirty",
+      " forty",
+      " fifty",
+      " sixty",
+      " seventy",
+      " eighty",
+      " ninety"
+  };
+
+
+
+
+  public String transformToString(String hours) {
+
+    String[] splitedString = hours.split(":");
+
+    int hour = Integer.parseInt(splitedString[0]);
+    int minute = Integer.parseInt(splitedString[1]);
+
+
+    String hourText = numberToString(hour);
+    String minuteText = numberToString(minute);
+    String result = "It's" + hourText + minuteText ;
+    return result;
+  }
+
+  private String numberToString(int number) {
+    String result;
+    if(number <= 19){
+      result = num[number];
+    } else {
+      result = num[number % 10];
+      number /= 10;
+
+      result = tens[number % 10] + result;
+    }
+    return result;
+  }
 }
--- wowcher-speaking-clock-exercise/src/test/java/SpeakingClockTest.java
+++ wowcher-speaking-clock-exercise/src/test/java/SpeakingClockTest.java
@@ -1,5 +1,20 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 
 public class SpeakingClockTest {
 
+  @Test
+  public void it_should_return_hour_to_string(){
+
+    SpeakingClock speakingClock = new SpeakingClock();
+
+    String hourInText = speakingClock.transformToString("08:34");
+    assertEquals("It's eight thirty four", hourInText);
+
+    assertEquals("It's nine thirty four", speakingClock.transformToString("09:34"));
+    assertEquals("It's fourteen five", speakingClock.transformToString("14:05"));
+    assertEquals("It's twenty three twenty four", speakingClock.transformToString("23:24"));
+  }
 }
